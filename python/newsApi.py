52db11506c3464068b88e2219a7931599cb4623c
georgiereeves <32246876+georgiereeves@users.noreply.github.com>
2018-04-18 11:25:21 +0100

    My resourceboard flask app in Heroku

--- python/newsApi.py
+++ python/newsApi.py
@@ -1,9 +1,11 @@
 from newsapi import NewsApiClient
+
 newsapi = NewsApiClient(api_key='0c0d7d15b5e74591b1c6a9c16a10545c')
 # To recieve the top headlines.
 
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
+top_headlines = newsapi.get_top_headlines(q='{}}',
                                           sources='bbc-news,the-verge',
                                           category='business',
                                           language='en',
                                           country='en')
+return requests.post
---
+++ python/spotify.py
@@ -0,0 +1,3 @@
+from spotifyapi import spotifyapi
+
+{ curl -X "GET" "https://api.spotify.com/v1/browse/categories/pop/playlists?country=UK" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQB6fWCz5t0eE6ztEKKm0m-pNRrlYb46HFeptYVGPfljnKZ2gfdNpsYwlI59YCZdmK37xd2FX3QvjARCqwXzLuXYJKrL8DpsUhIZG2FNHAZXlh4Z4hW_rJF6VdaK6OpKiSUItXgh5HHwHlKtnUO1"}
